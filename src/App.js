b899f9d782f473b83e771e51737072859569ec99
nattyjojo <nattyjojo9@gmail.com>
2023-08-23 17:51:45 +0200

    added footer content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import FooterContributor from "./components/footerContributors";
+import FooterContributor from "./components/footer/footerContributors";
+import anchors from "./components/footer/anchors";
 import "./index.css";
 import "./reset.css";
-import imageLinks from "./images/imageLink";
+//import imageLinks from "./images/imageLink";
 
 function App() {
   return <FooterContributor />;
---
+++ src/components/footer/footerContributors.js
@@ -0,0 +1,86 @@
+const FooterContributor = () => {
+  return (
+    <section className="figWrapper">
+      <h6 className="title">Contributors</h6>
+      <section className="footerFigContainer">
+        <section className="frontend">
+          <figure className="fig">
+            <a href="https://github.com/nattyjojo?tab=repositories">
+              <img
+                src="https://avatars.githubusercontent.com/u/85810900?v=4"
+                alt="john"
+                width="30px"
+              ></img>
+            </a>
+            <figcaption>JOHN CHIMEZIE</figcaption>
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </figure>
+          <figure className="fig">
+            <a href="https://github.com/nattyjojo?tab=repositories">
+              <img
+                src="https://avatars.githubusercontent.com/u/85810900?v=4"
+                alt="john"
+                width="30px"
+              ></img>
+            </a>
+            <figcaption className="fig">JOHN CHIMEZIE</figcaption>
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </figure>
+        </section>
+
+        <section className="backend">
+          <figure className="fig">
+            <a href="https://github.com/nattyjojo?tab=repositories">
+              <img
+                src="https://avatars.githubusercontent.com/u/85810900?v=4"
+                alt="john"
+                width="30px"
+              ></img>
+            </a>
+
+            <figcaption>JOHN CHIMEZIE</figcaption>
+
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </figure>
+          <figure className="fig">
+            <a href="https://github.com/nattyjojo?tab=repositories">
+              <img
+                src="https://avatars.githubusercontent.com/u/85810900?v=4"
+                alt="john"
+                width="30px"
+              ></img>
+            </a>
+
+            <figcaption>JOHN CHIMEZIE</figcaption>
+
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://avatars.githubusercontent.com/u/85810900?v=4">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </figure>
+        </section>
+      </section>
+      <hr></hr>
+      allright reseerved becode and friends
+    </section>
+  );
+};
+
+export default FooterContributor;
