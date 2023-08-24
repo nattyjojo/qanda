09df462b983e491d57d2a10a9c726e2255bb6f2b
nattyjojo <nattyjojo9@gmail.com>
2023-08-24 14:14:38 +0200

    added rigisteration form

---
+++ src/components/registerations/registerationComponent.js
@@ -0,0 +1,20 @@
+const RegisterationComponent = () => {
+  return (
+    <>
+      <h1 className="formTitle">BcodeQandA</h1>
+      <form>
+        <label>Username</label>
+        <br></br>
+        <input type="text" id="" name=""></input>
+        <br></br>
+        <label>Password </label>
+        <br></br>
+        <input type="password"></input>
+        <br></br>
+
+        <input className="formBtn" type="submit" value="submit"></input>
+      </form>
+    </>
+  );
+};
+export default RegisterationComponent;
